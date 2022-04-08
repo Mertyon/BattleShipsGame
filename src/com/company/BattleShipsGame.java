5c45c9aacf9f02423fee9e4b6cc962e6d4a32479
Mertyon <przemek_tru@wp.pl>
2022-04-08 18:33:11 +0200

    Tests failed.
    Tried to change row or column into destroyer1 = new char [] {'D','D','D','D'}
    Looking for another solution with copyOf / fill / etc

--- src/com/company/BattleShipsGame.java
+++ src/com/company/BattleShipsGame.java
@@ -13,43 +13,38 @@ public class BattleShipsGame {
         char water = '~';
         char ship = 's';
 
-        char battleShip = 'B';
+
         char destroyer1 = 'D';
-        char destroyer2 = 'd';
-        int [] test  = {'b', 'b', 'b', 'b'};
+        //char[] test  = {destroyer1, destroyer1, destroyer1, destroyer1};
 
         int shipNumber = 4;
         int gameBoardLenght = 10;
 
-       // int battleShipLenght = 5; leter
-      //  int destroyer1Lenght = 4;
-      //  int destroyer2Lenght = 4;
 
-
-        char [][] gameBoard = createGameBoard(gameBoardLenght, hit, miss, water, battleShip, destroyer1, destroyer2, shipNumber, ship);
+        char [][] gameBoard = createGameBoard(gameBoardLenght, water, destroyer1, ship);
         printGameBoard(gameBoard, water, ship);
         int undetectedShips = shipNumber;
         while(undetectedShips > 0){
             int[] guessLocation = getUserGuess(gameBoardLenght);
-            char locationVievUpdate = evaluateGuessAndGetTarget(guessLocation, gameBoard, water, ship, hit, miss, test);
-            if (locationVievUpdate == hit){
+            char locationViewUpdate = evaluateGuessAndGetTarget(guessLocation, gameBoard, water, ship, hit, miss, destroyer1);
+            if (locationViewUpdate == hit){
                 undetectedShips--;
             }
-            gameBoard = gameBoardUpdate(gameBoard, guessLocation, locationVievUpdate);
+            gameBoardUpdate(gameBoard, guessLocation, locationViewUpdate);
             printGameBoard(gameBoard, water, ship);
         }
         System.out.print("You won");
     }
 
-    private static char[][] gameBoardUpdate(char[][] gameBoard, int[] guessLocation, char locationVievUpdate) {
+    private static char[][] gameBoardUpdate(char[][] gameBoard, int[] guessLocation, char locationViewUpdate) {
         int row = guessLocation[0];
         int col = guessLocation[1];
-        gameBoard[row][col] = locationVievUpdate;
+        gameBoard[row][col] = locationViewUpdate;
         return  gameBoard;
 
     }
 
-    private static char evaluateGuessAndGetTarget(int[] guessLocation, char[][] gameBoard, char water, char ship, char hit, char miss, int[] test) {
+    private static char evaluateGuessAndGetTarget(int[] guessLocation, char[][] gameBoard, char water, char ship, char hit, char miss, char destroyer1) {
         String message;
         int row = guessLocation[0];
         int col = guessLocation[1];
@@ -58,9 +53,15 @@ public class BattleShipsGame {
         if (target == ship){
             target = hit;
             message = "Hit!";
+
+        } else if (target == destroyer1){
+            target = hit;
+            message = "Hit!";
+
         } else if (target == water){
             target = miss;
             message = "Miss!";
+            
         } else {
             message = "Already hit!";
         }
@@ -70,14 +71,14 @@ public class BattleShipsGame {
     }
 
     private static int[] getUserGuess(int gameBoardLenght) {
-        int row;
-        int col;
 
+        int row;
         do {
             System.out.print("Row: ");
             row = new Scanner(System.in).nextInt();
         } while (row < 1 || row > gameBoardLenght + 1);
 
+        int col;
         do {
             System.out.print("Column: ");
             col = new Scanner(System.in).nextInt();
@@ -97,7 +98,7 @@ public class BattleShipsGame {
 
         for (int row = 0; row < gameBoardLength; row++){
             System.out.print(row + 1 + " ");
-            for (int col = 0; col < gameBoardLength; col++){     // col = collumn
+            for (int col = 0; col < gameBoardLength; col++){     // col = column
                 char position = gameBoard[row][col];
                 if (position == ship){
                     System.out.print(water + " ");
@@ -107,33 +108,33 @@ public class BattleShipsGame {
         }
     }
 
-    private static char[][] createGameBoard(int gameBoardLength, char hit, char miss, char water, char battleShip, char destroyer1, char destroyer2, int shipNumber, char ship) {
+    private static char[][] createGameBoard(int gameBoardLength,char water, char destroyer1, char ship) {
         char [][] gameBoard = new char[gameBoardLength][gameBoardLength];
         for (char [] row : gameBoard){
             Arrays.fill(row, water);
-        } return placeShips(gameBoard, water, battleShip, destroyer1, destroyer2, shipNumber, ship);
+        } return placeShips(gameBoard, water, destroyer1, ship);
 
     }
 
-    private static char[][] placeShips(char[][] gameBoard, char water, char battleShip, char destroyer1, char destroyer2, int shipNumber, char ship) {
-        int placedDestroyers1 = 0;
-        int gameboardLenght = gameBoard.length;
+    private static char[][] placeShips(char[][] gameBoard, char water,char destroyer1, char ship ) {
+        int gameBoardLenght = gameBoard.length;
 
+        int placedDestroyers1 = 0;
         while (placedDestroyers1 <4){
-            int[] location = generateShipLocation(gameboardLenght);
-            char possibilityOfPlacment = gameBoard[location[0]][location[1]];
-            if (possibilityOfPlacment == water){
+            int[] location = generateShipLocation(gameBoardLenght);
+            char possibilityOfPlacement = gameBoard[location[0]][location[1]];
+            if (possibilityOfPlacement == water){
                 gameBoard[location[0]][location[1]] = destroyer1;
                 placedDestroyers1++;
             }
         } return gameBoard;
     }
 
-    private static int[] generateShipLocation(int gameboardLenght) {
+    private static int[] generateShipLocation(int gameBoardLenght) {
         int [] location = new int [2];
 
         for (int i = 0; i < location.length; i++){
-            location[i] = new Random().nextInt(gameboardLenght);
+            location[i] = new Random().nextInt(gameBoardLenght);
         } return location;
 
     }
